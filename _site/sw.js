702ac7def5fac19af049c5391f971363d961e4ee
snacky101 <snacky101@gmail.com>
2023-02-03 02:23:41 +0900

    Change avatar image

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230203.003349';
+const cacheName = 'chirpy-20230203.012245';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
