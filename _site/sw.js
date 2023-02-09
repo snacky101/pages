5e5ca1e192731d510cbdb5a34116bef60b08c720
snacky <>
2023-02-09 23:54:10 +0900

    Update post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230206.195316';
+const cacheName = 'chirpy-20230209.225349';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
