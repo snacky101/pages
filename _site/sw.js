0a1f7a408c2a25bb73f83c012d6946ebe7622ccf
snacky <>
2023-02-18 17:06:29 +0900

    Change avatar image

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230215.192038';
+const cacheName = 'chirpy-20230218.160550';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
