1719d8fdd6757b51f8dc92a18fc64ce232d1d0c6
snacky <>
2023-03-10 02:38:13 +0900

    Categorize a post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230307.212102';
+const cacheName = 'chirpy-20230310.013755';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
