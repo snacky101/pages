b3f23d680f23fa85fb834b6f75641cd215ee12cb
snacky101 <snacky101@gmail.com>
2023-03-14 21:32:53 +0900

    Update post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230310.013755';
+const cacheName = 'chirpy-20230314.203238';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
