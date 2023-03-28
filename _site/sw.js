8b2e43164c186229f279df064f9a6a9fce719d1c
snacky101 <snacky101@gmail.com>
2023-03-28 21:46:45 +0900

    Update book review post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230314.205749';
+const cacheName = 'chirpy-20230328.204631';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
